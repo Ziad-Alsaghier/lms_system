bdd8dea068d52fd13e8dd210efdb789e0cf54f11
Ziad <ziadm57@yahoo.com>
2025-03-16 23:59:35 +0200

    Edit Relation countTeacherSessionsEnded For Teacher

--- app/Models/User.php
+++ app/Models/User.php
@@ -115,7 +115,7 @@ class User extends Authenticatable
         }
         public function countTeacherSessionsEnded(){
             return $this->hasMany(SessionClass::class,
-            'teacher_id')->with(['package','student'])->whereNotNull('start')->whereNotNull('end')->count();
+            'teacher_id')->with(['package','student'])->whereNotNull('start')->whereNotNull('end');
         }
         public function sessions()
         {
