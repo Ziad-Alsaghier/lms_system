24eff145bf7686a4ba04d7226aa570dd717a442c
Ziad <ziadm57@yahoo.com>
2025-03-17 00:20:06 +0200

    Middleware Edit For Route Teacher

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ->prefix('admin')
         ->name('admin')
         ->group(base_path('routes/admin.php'));
-        Route::middleware(['api','auth:sanctum','IsAdmin'])
+        Route::middleware(['api','auth:sanctum','IsTeacher'])
         ->prefix('teacher')
         ->name('teacher')
         ->group(base_path('routes/teacher.php'));
